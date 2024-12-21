1f4329fc2981770ea6829e23d5cc7dd53520d208
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2024-12-21 12:21:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee=>{
+    if(employee.profession==="developer"){
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee=>{
+    if(employee.profession==="developer"){
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(employee => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "rishu", age: "22", profession: "marketeer" },
+    { id: 6, name: "raj", age: "25", profession: "manager" },
+    { id: 7, name: "singh", age: "24", profession: "clerk" },
+  ];
+  let concatenatedArray = arr.concat(newArr);
+  console.log(concatenatedArray);
 }
